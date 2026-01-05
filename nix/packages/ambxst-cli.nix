22e643228f21730f0f3f074209fa55c60927a492
Darsh yadav <darsh.yadav@proton.me>
2026-01-05 21:07:29 +0530

    make ambxst-cli the default

--- nix/packages/ambxst-cli.nix
+++ nix/packages/ambxst-cli.nix
@@ -2,6 +2,13 @@
   inherit (config.flake) mkCli;
 in {
   perSystem = { pkgs, self', ... }: {
-    packages.ambxst-cli = mkCli {inherit pkgs; shellPkg = self'.packages.ambxst;};
+    packages = {
+      ambxst-cli = mkCli {
+        inherit pkgs;
+        shellPkg = self'.packages.ambxst;
+      };
+
+      default = self'.packages.ambxst-cli;
+    };  
   };
 }
--- nix/packages/ambxst.nix
+++ nix/packages/ambxst.nix
@@ -1,8 +1,7 @@
 { config, inputs, ... }: let
   inherit (config.flake) mkAmbxst;
 in {
-  perSystem = { pkgs, self', ... }: {
+  perSystem = { pkgs, ... }: {
     packages.ambxst = mkAmbxst { inherit pkgs; src = inputs.ambxst; };
-    packages.default = self'.packages.ambxst;
   };
 }
